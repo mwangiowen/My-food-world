700acbde3dddd90ac977d027d4f01c2353a20c9a
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-12 23:14:38 +0300

    I have created a function to display search result

--- food.js
+++ food.js
@@ -1,3 +1,20 @@
 const foodContainer = document.getElementById('food-container');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
+function displaySearchResults(data) {
+    foodContainer.innerHTML = '';
+        if (data.meals) {
+        data.meals.forEach(meal => {
+            const foodCard = document.createElement('div');
+            foodCard.classList.add('food-card');
+            foodCard.innerHTML = `
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                <h2>${meal.strMeal}</h2>
+                <p>${meal.strInstructions}</p>
+            `;
+            foodContainer.appendChild(foodCard);
+        });
+    } else {
+        foodContainer.innerHTML = '<p>Oops! We don\'t have that food.</p>';
+    }
+}
