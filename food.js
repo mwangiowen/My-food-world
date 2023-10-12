4e066d2017cf9ca0655e7257629b4387fb745410
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-12 23:12:12 +0300

    on my js I just declared three variables

---
+++ food.js
@@ -0,0 +1,3 @@
+const foodContainer = document.getElementById('food-container');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
